d755e68c4b1753053b3372860107184ea256b559
Christopher Lakey <christopherlakey@gmail.com>
2013-10-07 02:02:30 -0400

    fixed bug with Boolean

--- src/main/java/com/scaleset/utils/BooleanUtils.java
+++ src/main/java/com/scaleset/utils/BooleanUtils.java
@@ -6,12 +6,10 @@ public class BooleanUtils {
      * Returns an Integer object holding the value of the specified String. If
      * the string is null or if there is an issue parsing the String, then the
      * default value is returned instead.
-     * 
-     * @param str
-     *            The string to coerce to a boolean
-     * @param fallback
-     *            The value to return if the input in null or cannot be coerced
-     *            to a boolean
+     *
+     * @param str      The string to coerce to a boolean
+     * @param fallback The value to return if the input in null or cannot be coerced
+     *                 to a boolean
      * @return The input coerced to a boolean or the fallback if the input
      *         cannot be coerced
      */
@@ -40,18 +38,18 @@ public class BooleanUtils {
      * the object is null or if there is an issue converting the object, then
      * the defalt value is returned instead. Supported types include: String,
      * Integer, Long
-     * 
-     * @param obj
-     *            The object to coerce to a boolean
-     * @param fallback
-     *            The value to return if the input in null or cannot be coerced
-     *            to a boolean
+     *
+     * @param obj      The object to coerce to a boolean
+     * @param fallback The value to return if the input in null or cannot be coerced
+     *                 to a boolean
      * @return The input coerced to a boolean or the fallback if the input
      *         cannot be coerced
      */
     public static Boolean valueOf(Object obj, Boolean fallback) {
         Boolean result = fallback == null ? null : fallback;
-        if (obj instanceof Integer) {
+        if (obj instanceof Boolean) {
+            result = (Boolean) obj;
+        } else if (obj instanceof Integer) {
             result = ((Integer) obj != 0);
         } else if (obj instanceof String) {
             try {
